9c9781ff4fea580ffc6c3d51a206f1eca05e3131
Main-Michael-Sparks <anti.michael89@gmail.com>
2022-04-21 14:02:18 -0400

    Db connection pass

--- back-end/src/db/seeds/00-reservations.js
+++ back-end/src/db/seeds/00-reservations.js
@@ -1,3 +1,5 @@
+const seeds = require("./00-reservations.json");
 exports.seed = function (knex) {
-  return knex.raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE");
+  return knex.raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE")
+    .then(()=> knex.insert(seeds).into("reservations"))
 };
--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,9 +1,11 @@
 /**
  * List handler for reservation resources
  */
+const knex = require("../db/connection");
 async function list(req, res) {
+  const data = await knex("reservations").select("*");
   res.json({
-    data: [],
+    data
   });
 }
 
